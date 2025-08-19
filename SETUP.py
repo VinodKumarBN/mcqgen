4dbf7ed58d0e89ac104a255446d41e9cf91e96d1
Vinod Kumar B N <vinodvinodkumarbn164@gmail.com>
2025-08-19 15:29:34 +0530

    file updated

---
+++ SETUP.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Vinod Kumar B N',
+    author_email='vinodvinodkumarbn164@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
