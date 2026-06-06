a8aec684f673b647b6be04b3892d6144ad751dab
Vinod kumar B N <vinodvinodkumarbn164@gmail.com>
2026-06-06 09:25:47 +0530

    Fix import path for PromptTemplate in StreamlitAPP.py

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 import traceback
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
+langchain_core.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import PyPDF3 
 from langchain.schema.runnable import RunnableSequence
